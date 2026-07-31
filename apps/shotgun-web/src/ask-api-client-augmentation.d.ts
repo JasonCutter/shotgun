a81bb00721b2a3a60fd415f680b278620458dab3
JasonCutter <lhm2466@gmail.com>
2026-07-31 17:39:57 +0900

    expose ask contract types to frontend slice

---
+++ apps/shotgun-web/src/ask-api-client-augmentation.d.ts
@@ -0,0 +1,4 @@
+declare module '@shotgun/api-client' {
+  export type AskMode = import('../../../packages/contracts/src/index.js').AskMode;
+  export type AskWorkspaceView = import('../../../packages/contracts/src/index.js').AskWorkspaceView;
+}

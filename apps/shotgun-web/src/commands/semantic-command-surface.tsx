7e02498ab79e42af66ee512b8a30c2a73b176144
JasonCutter <lhm2466@gmail.com>
2026-09-08 21:40:49 +0900

    chore: remove unused semantic command import

--- apps/shotgun-web/src/commands/semantic-command-surface.tsx
+++ apps/shotgun-web/src/commands/semantic-command-surface.tsx
@@ -1,4 +1,4 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { useEffect, useId, useState } from 'react';
 
 import type { GlobalShellView, SemanticComparisonStatusView } from '@shotgun/api-client';
